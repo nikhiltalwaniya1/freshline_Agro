e403eff74af5860e74837d3ea8c998f5bfa09f01
nikhiltalwaniya1 <125686853+nikhiltalwaniya1@users.noreply.github.com>
2024-01-19 16:14:01 +0530

    change in check forms

--- utills/checkForms.js
+++ utills/checkForms.js
@@ -43,6 +43,70 @@ const formNames = [
     formName: "RST02Raw_Material_Stock_and_Issue_Register",
     path: "/materialStockandIssueRegister"
 
+  },
+  {
+    formName: "FST03Material_Issue_Slip",
+    path: "/material-issue-slip"
+  },
+  {
+    formName: "FPRD07Chlorine_Concentration_Record",
+    path: "/chlorine-concentration-record "
+  },
+  {
+    formName: "RPRD05Boiler_Temp_Record",
+    path: "/boiler-temp-record"
+  },
+  {
+    formName: "RPRD05Belt_Dryer_Temperature_Record",
+    path: "/belt-dryer-temperature-record"
+  },
+  {
+    formName: "FPRD02_Work_in_Process_Report_for_Belt_Dryer",
+    path: "/belt-dryer-work-report"
+  },
+  {
+    formName: "RPRD04Control_of_Nonconforming_Product_Register",
+    path: "/nonconforming-product-register"
+  },
+  {
+    formName: "FPRD14_UV_Light_Monitoring_Record",
+    path: "/uv-light-monitoring-record"
+  },
+  {
+    formName: "FQC03Certificate_of_Analysis_COA",
+    path: "/certificate-analysis-coa"
+  },
+  {
+    formName: "FPRD13_Line_Clearance_Record_after_Manufacturing",
+    path: "/line-clearance-record-manufacturing"
+  },
+  {
+    formName: "FPRD08Daily_Metal_Detector_Test_Log",
+    path: "/metal-detector-test"
+  },
+  {
+    formName: "RPRD03Metal_Detecting_Log_Sheet",
+    path: "/metal-detecting-log-sheet"
+  },
+  {
+    formName: "FPRD05Finished_Goods_Packing_Report",
+    path: "/goods-packing-report"
+  },
+  {
+    formName: "FQC10_Packing_checklist",
+    path: "/packing-checklist"
+  },
+  {
+    formName: "FPRD12_Line_Clearance_Record_After_Packing",
+    path: "/line-clearance-record-packing"
+  },
+  {
+    formName: "RST04_Dispatch_Register_For_Finished_Goods",
+    path: "/register-for-finished-goods"
+  },
+  {
+    formName: "FQC11_outward_Vehicle_Checklist",
+    path: "/outward-vehicle-checklist"
   }
 ]
 exports.checkForms = async () => {
@@ -77,16 +141,16 @@ exports.checkForms = async () => {
 
 const roleName = [
   {
-    rolename:"Super Admin",
-    roletype:0
+    rolename: "Super Admin",
+    roletype: 0
   },
   {
-    rolename:"Admin",
-    roletype:1
+    rolename: "Admin",
+    roletype: 1
   },
   {
-    rolename:"User",
-    roletype:2
+    rolename: "User",
+    roletype: 2
   }
 ]
 
@@ -97,8 +161,8 @@ exports.checkRole = async () => {
       const roleDetails = await roleModel.findOne({ rolename: roleNameDetails.rolename }).lean();
       if (roleDetails === null) {
         const obj = {
-          rolename:roleNameDetails.rolename,
-          roletype:roleNameDetails.roletype
+          rolename: roleNameDetails.rolename,
+          roletype: roleNameDetails.roletype
         }
         const saveRole = new roleModel(obj)
         await saveRole.save()
